66250000b6d99890c362b1e8f0fc99e2cf9a79da
Claudia Palazon <claupanon@gmail.com>
2020-11-25 09:59:13 +0100

    fixed comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// Este es el archivo desde el que comienza a pintarse la web. Recoge el div "Root", donde pintará lo que queremos.
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
